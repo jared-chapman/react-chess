aec646ad77cc3de2667da4e9529684bc29865600
Jared Chapman <jared.chapman@lossexpress.com>
2021-12-31 12:56:12 -0600

    updated piece objects with color/images

--- src/index.js
+++ src/index.js
@@ -1,71 +1,145 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import blackKing from './piece_images/blackKing.png'
+import blackQueen from './piece_images/blackQueen.png'
+import blackRook from './piece_images/blackRook.png'
+import blackBishop from './piece_images/blackBishop.png'
+import blackKnight from './piece_images/blackKnight.png'
+import blackPawn from './piece_images/blackPawn.png'
+import whiteKing from './piece_images/whiteKing.png'
+import whiteQueen from './piece_images/whiteQueen.png'
+import whiteRook from './piece_images/whiteRook.png'
+import whiteBishop from './piece_images/whiteBishop.png'
+import whiteKnight from './piece_images/whiteKnight.png'
+import whitePawn from './piece_images/whitePawn.png'
+
 
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 //Initialize pieces
-const pawn = {
+const pawnB = {
   name: "Pawn",
   hasMoved: false,
   range: 2,
   r: 1,
   c: 1,
-  image: "<img src=/pawn.png>",
+  image: blackPawn,
   availableSquares(position) {
       return position;
   }
-
 }
-const knight = {
+const pawnW = {
+  name: "Pawn",
+  hasMoved: false,
+  range: 2,
+  r: 1,
+  c: 1,
+  image: whitePawn,
+  availableSquares(position) {
+      return position;
+  }
+}
+const knightB = {
   name: "Knight",
   r: 1,
   c: 1,
+  image: blackKnight,
   availableSquares(position) {
       return position;
   }
 }
-const bishop = {
+const knightW = {
+  name: "Knight",
+  r: 1,
+  c: 1,
+  image: whiteKnight,
+  availableSquares(position) {
+      return position;
+  }
+}
+const bishopB = {
   name: "Bishop",
   r: 1,
   c: 1,
+  image: blackBishop,
   availableSquares(position) {
       return position;
   }
 }
-const rook = {
+const bishopW = {
+  name: "Bishop",
+  r: 1,
+  c: 1,
+  image: whiteBishop,
+  availableSquares(position) {
+      return position;
+  }
+}
+const rookB = {
   name: "Rook",
   r: 1,
   c: 1,
+  image: blackRook,
   availableSquares(position) {
       return position;
   }
 }
-const queen = {
+const rookW = {
+  name: "Rook",
+  r: 1,
+  c: 1,
+  image: whiteRook,
+  availableSquares(position) {
+      return position;
+  }
+}
+const queenB = {
   name: "Queen",
   r: 1,
   c: 1,
+  image: blackQueen,
   availableSquares(position) {
       return position;
   }
 }
-const king = {
+const queenW = {
+  name: "Queen",
+  r: 1,
+  c: 1,
+  image: whiteQueen,
+  availableSquares(position) {
+      return position;
+  }
+}
+const kingB = {
   name: "King",
   r: 1,
   c: 1,
+  image: blackKing,
+  availableSquares(position) {
+      return position;
+  }
+}
+const kingW = {
+  name: "King",
+  r: 1,
+  c: 1,
+  image: whiteKing,
   availableSquares(position) {
       return position;
   }
 }
 
-const startingPiecePositions = [[rook, knight, bishop, queen, king, bishop, knight, rook],
-                             [pawn, pawn,   pawn,   pawn,  pawn, pawn,   pawn,   pawn],
-                             ["", "",   "",   "",  "", "",   "",   ""],
-                             ["", "",   "",   "",  "", "",   "",   ""],
-                             ["", "",   "",   "",  "", "",   "",   ""],
-                             ["", "",   "",   "",  "", "",   "",   ""],
-                             [pawn, pawn,   pawn,   pawn,  pawn, pawn,   pawn,   pawn],
-                             [rook, knight, bishop, queen, king, bishop, knight, rook]]
+const startingPiecePositions = [[rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB],
+                                [pawnB, pawnB,   pawnB,   pawnB,  pawnB, pawnB,   pawnB,   pawnB],
+                                ["",     "",     "",     "",    "",   "",     "",     ""],
+                                ["",     "",     "",     "",    "",   "",     "",     ""],
+                                ["",     "",     "",     "",    "",   "",     "",     ""],
+                                ["",     "",     "",     "",    "",   "",     "",     ""],
+                                [pawnW, pawnW,   pawnW,   pawnW,  pawnW, pawnW,   pawnW,   pawnW],
+                                [rookW, knightW, bishopW, queenW, kingW, bishopW, knightW, rookW]];
+
 
 class Square extends React.Component {
     constructor(props){
@@ -85,8 +159,16 @@ class Square extends React.Component {
             <button 
                 style = {{background: "#227F32" }}
                 className="square" 
+                onClick = {() => {
+                  // Trying to move a piece
+                  // console.log("click");
+                  // console.log(this.state.r);
+                  // this.state.r += 1;
+                  // this.state.c += 1;
+                }}
                 >
-            {this.state.p.name}
+            {<img src={this.state.p.image} ></img>}
+            
             </button>
         ); 
     } else {
@@ -95,7 +177,7 @@ class Square extends React.Component {
                 style = {{background: "#D5C798" }}
                 className="square"
                 >
-                {this.state.p.name}
+                {<img src={this.state.p.image} ></img>}
             </button>
             );   
     }
